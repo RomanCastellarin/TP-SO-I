397429cd1fdc073457de0d8bb0cabce754bd21fd
Juan Ignacio Suarez <juaniisuar@gmail.com>
2018-06-12 16:11:36 -0300

    Update generate_n_nodes.sh
    Now they ping themselves to connect themselves between each other.

--- Misc/generate_n_nodes.sh
+++ Misc/generate_n_nodes.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-# Usage: ./generate_n_nodes.sh N_SERVERS
+# Usage: ./generate_n_nodes.sh N_SERVERS COOKIE
 
-n_servers=$1
-cookie=secret
-lan_ip="$(ifconfig eth0 | grep -Eo '(inet:)([0-9]*\.){3}[0-9]*' | cut -c 6-)"
-pids=()
+N_SERVERS=$1
+COOKIE=$2
+HOST_IP="$(ifconfig eth0 | grep -Eo '(inet:)([0-9]*\.){3}[0-9]*' | cut -c 6-)"
+PIDS=()
 
-for i in $(seq 0 "$(($n_servers-1))"); do
-	erl -name server$i@$lan_ip -setcookie $cookie -noshell &
-	pids[$i]=$!
-	done
+for i in $(seq 0 "$(($N_SERVERS-1))"); # for(i=0; i<N_SERVERS; i++)
+do
+	if [ $i -eq 0 ]	
+	then # It's the first server
+		erl -name server$i@$HOST_IP -setCOOKIE $COOKIE -noshell &
+	else # Ping myself from the previous generated server 
+		erl -name server$i@$HOST_IP -setCOOKIE $COOKIE -noshell -eval 'rpc:call(server$(($i-1))@$HOST_IP, net_adm, ping, [server$i@$HOST_IP]).' &
+	fi
+	PIDS[$i]=$! # Save node PID
+done
 
-echo "${pids[*]}"
-for pid in "${pids[*]}"; do
+echo "${PIDS[*]}" # Echo background PIDs
+for pid in "${PIDS[*]}"; do # Kill the nodes (for testing)
 	kill $pid
 	done

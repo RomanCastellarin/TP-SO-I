19a0459217f3957f880d9590208ab0bb04120c8d
RomanCastellarin <RomanCastellarin@users.noreply.github.com>
2018-06-12 18:01:34 -0300

    added pstat and pbalance

--- Source/server.erl
+++ Source/server.erl
@@ -3,6 +3,19 @@
 -compile(export_all).
 -define(PORT, 5423).
 
+wait(X) -> receive after (1000*X) -> ok end.
+
+getMinState(D) ->
+	Comp = fun (Node, Load, {OldNode, OldLoad}) ->
+		if Load < OldLoad -> {Node, Load};
+		   true			  -> {OldNode, OldLoad} end end,
+	{Node,_} = orddict:fold(Comp, {none, 1.0e10}, D),
+    Node.
+
+updateStates(D, K, V) ->
+	orddict:update(K, fun (_)->V end, V, D).
+
+% NetworkNode: 'pepe@192.168.15.05'
 server(Port, NetworkNode) ->
 	case net_adm:ping(NetworkNode) of
 		pong -> server(Port);
@@ -13,6 +26,7 @@ server(Port) ->
 	case gen_tcp:listen(Port, [{active, false}]) of 
 		{ok, ListenSocket} ->
 			spawn(?MODULE, dispatcher, [ListenSocket, 0]),
+			% all the other processes			
 			ok;
 		{error, ErrorMessage} ->
 			io:format("No se pudo crear la conexión, ~p.~n", [ErrorMessage]),
@@ -25,10 +39,24 @@ dispatcher(ListenSocket, N) ->
 	dispatcher(ListenSocket, N+1).
 	
 	
-pBalance() ->
-	Nodes = [ node() | nodes() ],
-	ok.
+pBalance(NodesState) ->
+    io:format("~p~n", [NodesState]),
+	receive
+		{getMin, Pid} ->
+			Pid ! getMinState(NodesState),
+			pBalance(NodesState);
+		{notify, Node, Load} ->
+			pBalance(updateStates(NodesState, Node, Load))
+	end.
+
+
+pStat() ->
+	{_,Load} = statistics(reductions),
+	lists:foreach(fun(Node) -> {balance, Node} ! {notify, node(), Load} end, [node()|nodes()]),
+	wait(1),
+	pStat().
 	
+
 pSocket(Socket, N) -> 
 	case gen_tcp:recv(Socket, 0) of
 		{ok, Message} ->
@@ -42,6 +70,11 @@ pSocket(Socket, N) ->
 	end,
 	ok.
 	
+funy() ->
+	{A, B} = statistics(reductions),
+	io:format("~p~n", [B]),
+	funy().
 
-	
-
+funny() -> 
+    register(balance, spawn(?MODULE, pBalance, [orddict:new()])),
+    spawn(?MODULE, pStat, []).
